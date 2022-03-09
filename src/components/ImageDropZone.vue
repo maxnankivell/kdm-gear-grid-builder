51a56560d727e80012fd9e4e48c165472aac1ad2
maxnankivell <maxnankivell1@gmail.com>
2022-03-10 02:59:41 +1300

    fixed selection problem

--- src/components/ImageDropZone.vue
+++ src/components/ImageDropZone.vue
@@ -114,11 +114,6 @@ function onDragEnd(event: DragEvent): void {
 }
 
 .no-drag {
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -o-user-select: none;
-  user-select: none;
   -webkit-user-drag: none;
   -khtml-user-drag: none;
   -moz-user-drag: none;
